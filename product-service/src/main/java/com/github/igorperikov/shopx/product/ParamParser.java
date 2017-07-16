cf3b9080e304e4a14e30cc751f2d1c84f475a595
IgorPerikov <perikov.igor@gmail.com>
2017-07-16 21:37:34 +0300

    add support for get /products with pagination

---
+++ product-service/src/main/java/com/github/igorperikov/shopx/product/ParamParser.java
@@ -0,0 +1,26 @@
+package com.github.igorperikov.shopx.product;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.ext.web.RoutingContext;
+
+import java.util.Optional;
+
+public class ParamParser {
+    public static Integer getIntValue(RoutingContext rc, String paramName, String defaultValue) {
+        return Integer.valueOf(
+                Optional.ofNullable(rc.request().getParam(paramName)).orElse(defaultValue)
+        );
+    }
+
+    public static Integer getIntValue(RoutingContext rc, String paramName) {
+        String paramValue = rc.request().getParam(paramName);
+        if (paramValue == null) {
+            rc.response()
+                    .setStatusCode(400)
+                    .end(new JsonObject().put("description", "specify '" + paramName + "' query param").encodePrettily());
+            return 0;
+        } else {
+            return Integer.valueOf(paramValue);
+        }
+    }
+}
--- product-service/src/main/java/com/github/igorperikov/shopx/product/ProductVerticle.java
+++ product-service/src/main/java/com/github/igorperikov/shopx/product/ProductVerticle.java
@@ -1,6 +1,7 @@
 package com.github.igorperikov.shopx.product;
 
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -11,9 +12,11 @@ import io.vertx.rxjava.ext.web.Router;
 import io.vertx.rxjava.ext.web.RoutingContext;
 import rx.Single;
 
+
 public class ProductVerticle extends AbstractVerticle {
     private static final Logger log = LoggerFactory.getLogger(ProductVerticle.class);
     private static final Integer PORT = 8080;
+    private static final String SQL_GET_PAGE_OF_PRODUCTS = "SELECT * FROM products limit ?,?";
 
     private JDBCClient dbClient;
 
@@ -30,9 +33,15 @@ public class ProductVerticle extends AbstractVerticle {
         });
     }
 
+    @Override
+    public void stop(Future<Void> stopFuture) throws Exception {
+        dbClient.rxClose().subscribe(aVoid -> stopFuture.complete());
+    }
+
     private Future<Void> startWebServer() {
         Router router = Router.router(vertx);
-        router.get("/ping").handler(this::ping);
+        router.get("/products").handler(this::returnProducts);
+        router.post("/products").handler(this::createProduct);
 
         return Future.future(event -> {
             vertx.createHttpServer()
@@ -45,15 +54,35 @@ public class ProductVerticle extends AbstractVerticle {
         });
     }
 
-    private void ping(RoutingContext rc) {
-        rc.response()
-                .putHeader("Content-type", "application/json")
-                .end(new JsonObject().put("status", "ok").encodePrettily());
+    private void returnProducts(RoutingContext rc) {
+        // TODO: move to rx?
+        Integer page = ParamParser.getIntValue(rc, "page", "1");
+        Integer rowCount = ParamParser.getIntValue(rc, "per_page", "10");
+        Integer rowOffset = rowCount * (page - 1);
+        dbClient.rxGetConnection()
+                .flatMap(sqlConnection -> {
+                    return sqlConnection.rxQueryWithParams(
+                            SQL_GET_PAGE_OF_PRODUCTS,
+                            new JsonArray().add(rowOffset).add(rowCount)
+                    ).doAfterTerminate(sqlConnection::close);
+                })
+                .subscribe(
+                        resultSet -> {
+                            rc.response()
+                                    .putHeader("Content-type", "application/json")
+                                    .end(new JsonObject().put("products", new JsonArray(resultSet.getRows())).encodePrettily());
+                        },
+                        rc::fail
+                );
+    }
+
+    private void createProduct(RoutingContext rc) {
+        rc.response().end(new JsonObject().put("function", "save new").encodePrettily());
     }
 
     private Future<Void> obtainDatabaseConnection() {
         return Future.future(event -> {
-            dbClient = JDBCClient.createNonShared(vertx, new JsonObject()
+            dbClient = JDBCClient.createShared(vertx, new JsonObject()
                     .put("provider_class", "io.vertx.ext.jdbc.spi.impl.HikariCPDataSourceProvider")
                     .put("jdbcUrl", "jdbc:mysql://localhost:3306/shopx?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useSSL=false")
                     .put("username", "root")
